283c74a6d75fc352433af3a05ce6e0c03c2f831e
CodeMaestroJuan <slimbunniee@gmail.com>
2024-07-21 16:34:26 -0500

    adinviaste perrita

--- script.js
+++ script.js
@@ -10,11 +10,16 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No hay número 😔👌';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      'Adivinaste Perrita! 🎉💖🐕';
   }
 });
