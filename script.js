6937438c8225d2a5cc1e7ef76ad76d6924992905
CodeMaestroJuan <slimbunniee@gmail.com>
2024-07-22 18:41:00 -0500

    empy space

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = '';
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
 
